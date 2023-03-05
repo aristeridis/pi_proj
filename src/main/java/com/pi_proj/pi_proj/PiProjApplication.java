cb7e45d8ced9795af153f207510cb80bf9055c4b
Alexandros <aristeridis@hotmail.gr>
2023-03-05 17:12:05 +0200

    Initial commit with added pom dependencies

---
+++ src/main/java/com/pi_proj/pi_proj/PiProjApplication.java
@@ -0,0 +1,13 @@
+package com.pi_proj.pi_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiProjApplication.class, args);
+    }
+
+}
